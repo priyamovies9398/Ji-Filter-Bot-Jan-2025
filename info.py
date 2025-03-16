f79e7d4cc329578223137c954742f226c9146e7b
PriyaBots <158731094+priyamovies9398@users.noreply.github.com>
2025-03-16 17:21:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #----------- There will be channel id add in all these ---------
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1001981616450'))  # set shortner log channel
-DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0')) # The movie you upload in it will be deleted from the bot.
+DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','-1002616156942')) # The movie you upload in it will be deleted from the bot.
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
 auth_channel = environ.get('AUTH_CHANNEL', '-1002095552479')
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002095552479'))
